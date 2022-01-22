6c2d3bf6687f3208c2e1db677a4582faedd96035
Doğukan YILDIZ <dogukanyildiz99@gmail.com>
2022-01-22 20:15:35 +0300

    Main Menu ve Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Scene yüklemek için
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    //Oyunun durdurulup durdurulmadığını kontrol etmek için
+    public static bool OyunDurduruldu = false;
+
+    //UI kontrolü için
+    public GameObject molaMenuUI;
+
+    //Oyunu devam ettirir
+    public void Devam()
+    {
+        molaMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        OyunDurduruldu = false;
+    }
+    //Oyunu durdurur
+    public void Durdur()
+    {
+        molaMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        OyunDurduruldu = true;
+    }
+
+    //Ana menüye dönüşü sağlar
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Start Screen");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Esc tuşuna basılınca oyun durdurulmalı
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Esc tuşuna tekrar basarak oyunu devam ettirebiliriz
+            if(OyunDurduruldu)
+            {
+                Devam();
+            }
+            else
+            {
+                Durdur();
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/SaveGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadGame : MonoBehaviour
+{
+    //Kayıtlı oyuna devam etmemizi sağlar
+    public void Continue()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
